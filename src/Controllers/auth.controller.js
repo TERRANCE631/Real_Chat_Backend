9c4c104673f057c0e3216c5d4ae6dc3032dad837
TERRANCE631 <tarrancesiyabonga@gmail.com>
2025-07-12 17:32:46 +0200

    still on auth

--- src/Controllers/auth.controller.js
+++ src/Controllers/auth.controller.js
@@ -1,32 +1,55 @@
 import { db } from "../Lib/db.js";
 import { GenerateToken } from "../Lib/utils.js";
+import bcrypt from "bcryptjs";
+
 import { userQuery__credantials, userQuery__table, userQuery__checkUser, userQuery__getUserID } from "../Models/user.model.js";
 
 export const signup = (req, res) => {
     const { username, email, password } = req.body;
-    const values = [username, email, password];
+
+    const salt = bcrypt.genSaltSync(10)
+    const hashedPassword = bcrypt.hashSync(password, salt);
+
+    const values = [
+        username,
+        email,
+        req.body.password = hashedPassword
+    ];
 
     db.query(userQuery__table)
-    db.query(userQuery__checkUser, (err, data) => {
-        const findEmail = data.find(e => e.email.includes(email));
+    db.query(userQuery__checkUser, (err, allUsers) => {
+        const findEmail = allUsers.find(e => e.email.includes(email));
 
         if (!findEmail) {
             db.query(userQuery__credantials, [values])
-
-            db.query(userQuery__getUserID, email, () => {
-                if (data.length > 0) {
-                    res.json({ testing: data[0].id })
-                }
-            })
         } else {
-            res.json({ message: "User already exist" })
-        }
-    })
+            res.json({ message: "User already exist" });
+        };
+
+        if (allUsers.length > 0) return
+        db.query(userQuery__getUserID, email, (err, selectedUser) => {
+            if (selectedUser.length > 0) {
+                GenerateToken(selectedUser[0].id, res)
+                res.json({ user: selectedUser[0] })
+            }
+        });
+    });
 };
 
 export const loggin = (req, res) => {
-    res.json({ text: "Logging in..." })
+    const { email, password } = req.body;
+
+    db.query(userQuery__getUserID, email, (err, user) => {
+        const isPasswordCorrect = bcrypt.compareSync(password, user[0].password);
+        if (isPasswordCorrect) {
+            GenerateToken(user[0].id, res);
+            res.json({ user: user[0] });
+        } else {
+            return res.status(400).json("Invalid credentials")
+        };
+    });
 };
+
 export const loggout = (req, res) => {
     res.json({ text: "Logging out..." })
 }; 
