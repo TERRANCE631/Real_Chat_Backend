7c5ab58049abb91a1066a9393717b1ad9c15b6a2
TERRANCE631 <tarrancesiyabonga@gmail.com>
2025-10-29 17:12:51 +0200

    handling .env again

--- src/Lib/utils.js
+++ src/Lib/utils.js
@@ -10,20 +10,20 @@ export const GenerateToken = (userID, res) => {
     const token = jwt.sign({ userID }, secret, { expiresIn: "7d" });
 
     // production cookie if needed in future like cross site
-    // res.cookie("token", token, {
-    //     httpOnly: true,
-    //     sameSite: "none", // must be none on production 
-    //     secure: true, // must be true on production
-    //     maxAge: 1000 * 60 * 60 * 24 * 7
-    // });
-
-    // development cookie
     res.cookie("token", token, {
         httpOnly: true,
-        sameSite: "lax", // must be none on localhost
-        secure: false, // must be false on localhost
-        maxAge: 7 * 24 * 60 * 60 * 1000,
+        sameSite: "none", // must be none on production 
+        secure: true, // must be true on production
+        maxAge: 1000 * 60 * 60 * 24 * 7
     });
+
+    // development cookie
+    // res.cookie("token", token, {
+    //     httpOnly: true,
+    //     sameSite: "lax", // must be none on localhost
+    //     secure: false, // must be false on localhost
+    //     maxAge: 7 * 24 * 60 * 60 * 1000,
+    // });
 
     // Server-side debug
     console.log("✅ Token generated:", token);
